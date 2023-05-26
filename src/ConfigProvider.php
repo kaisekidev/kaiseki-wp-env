1ae566153718985c1b03e74ff4cf93152857c0fa
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-05-26 13:01:38 +0200

    add config provider

---
+++ src/ConfigProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaiseki\WordPress\Environment;
+
+final class ConfigProvider
+{
+    /**
+     * @return array<mixed>
+     */
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                'aliases' => [
+                    EnvironmentInterface::class => Environment::class,
+                ],
+            ],
+        ];
+    }
+}
